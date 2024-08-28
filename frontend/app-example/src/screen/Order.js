4a4ef9c65555579e77550a175adc1b019e6a8bda
manuelalejandroln13 <manuelalejandroln13@gmail.com>
2024-08-28 04:41:55 -0500

    nueva pantalla prueba

--- frontend/app-example/src/screen/Order.js
+++ frontend/app-example/src/screen/Order.js
@@ -1,191 +1,133 @@
-import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Button, TextInput } from "react-native";
-import { Chip } from "react-native-paper";
-import * as ExpoDocumentPicker from "expo-document-picker";
-import * as FileSystem from "expo-file-system";
+import { Link } from "expo-router";
+import React, {useState} from "react";
+import {Text, View, StyleSheet, TouchableOpacity, TextInput, FlatList } from "react-native";
 
-const serverIP = process.env.EXPO_PUBLIC_ServerIP;
 
-const Classifier = () => {
-  const [file, setFile] = useState({ canceled: true });
-  const [result, setResult] = useState("");
-  const [chips, setChips] = useState([]);
-  const [inputTags, setInputTags] = useState("");
-  const [charge, setCharge] = useState(true);
+const Order = () => {
+    const [llamada, setLlamada] = useState('');
+    const [valor, setValor] = useState('');
+    const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    const defaultTags = [
-      { id: 1, text: "Cine", selected: true },
-      { id: 2, text: "Politica", selected: true },
-      { id: 3, text: "Religion", selected: true },
-    ];
-    setChips(defaultTags);
-  }, []);
+    const getResultFromClasificar = async () => {
+      try {
+        const dataToSend = { llamada, valor };
+        console.log("JSON to send:", JSON.stringify(dataToSend, null, 2));
+  
+  
+        const response = await fetch("http://192.168.1.159:3000/openapi", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ llamada, valor }),
+        });
+        const jsonData = await response.json();
+        console.log("JSON received:", JSON.stringify(jsonData, null, 2));
+        // Supongo que jsonData tiene la estructura { historial: [...] }
+        setResults(jsonData || []);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          {"Ingrese el texto que desea conocer la clasificacion"}
+        </Text>
+        
+        <TextInput 
+          style={styles.input} 
+          value={llamada} 
+          onChangeText={setLlamada} 
+          placeholder="Ingrese el texto" 
+          placeholderTextColor="#888"
+        />
+        <TextInput 
+          style={styles.input} 
+          value={valor} 
+          onChangeText={setValor} 
+          placeholder="Ingrese el valor" 
+          placeholderTextColor="#888"
+        />
+        
+        <TouchableOpacity style={styles.button} onPress={getResultFromClasificar}>
+          <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableOpacity>
 
-  const handleFilePicker = async () => {
-    const document = await ExpoDocumentPicker.getDocumentAsync({
-      type: "application/pdf",
-      multiple: false,
-    });
-    setFile(document);
-  };
-
-  const handleChipPress = (chipId) => {
-    setChips((prevChips) =>
-      prevChips.map((chip) => {
-        if (chip.id === chipId) {
-          return { ...chip, selected: !chip.selected };
-        }
-        return chip;
-      })
+        <Link href="/home" style={styles.button} >
+          <Text style={styles.buttonText}>Regresar</Text>
+        </Link>
+        
+        <FlatList
+                data={results}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.resultContainer}>
+                        <Text style={styles.result}>Label: {item.label}</Text>
+                        <Text style={styles.result}>Valor: {item.valor}</Text>
+                    </View>
+                )}
+                ListEmptyComponent={<Text style={styles.result}>No hay resultados.</Text>}
+            />
+      </View>
     );
   };
 
-  const handleTagsInputChange = (text) => {
-    setInputTags(text);
-  };
-
-  const handleUpload = async () => {
-    if (file.canceled) {
-      console.error("Seleccione un pdf");
-    } else {
-      try {
-        // Upload the selected file
-        await FileSystem.uploadAsync(
-          `http://${serverIP}/upload`,
-          file.assets[0].uri,
-          {
-            fieldName: file.assets[0].name,
-            httpMethod: "POST",
-            headers: {
-              "Content-Type": "multipart/form-data",
-              name: file.assets[0].name,
-            },
-            uploadType: FileSystem.FileSystemUploadType.BINARY_CONTENT,
-          }
-        );
-
-        // Fetch text content from server
-        const queryParams = new URLSearchParams({
-          name: file.assets[0].name,
-        }).toString();
-        const response = await fetch(
-          `http://${serverIP}/getText?${queryParams}`,
-          {
-            method: "GET",
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Error de red o servidor");
-        }
-
-        const text = await response.text();
-
-        // Prepare data for classification
-        const selectedTags = chips.filter((chip) => chip.selected).map((chip) => chip.text);
-        const combinedTags = [...selectedTags, ...inputTags.split(",").map((tag) => tag.trim())];
-        const data = {
-          text: text,
-          labels: combinedTags,
-        };
-
-        // Call classifier API
-        const classifierResponse = await fetch(
-          "http://192.168.1.159:3000/classifier",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-          }
-        );
-
-        const classifierResult = await classifierResponse.json();
-        let show = "";
-        for (let i = 0; i < classifierResult.labels.length; i++) {
-          const score = classifierResult.scores[i] * 100;
-          show += `${classifierResult.labels[i]}   -   ${score.toFixed(2)}%\n`;
-        }
-        setResult(show);
-        setFile({ canceled: true });
-      } catch (error) {
-        setCharge(false);
-        console.error("Error:", error);
-      }
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.containerButton}>
-        <Button title={"Selecciona el PDF"} onPress={handleFilePicker} />
-      </View>
-      <Text style={styles.text}>Seleccione las etiquetas de clasificación:</Text>
-      <View style={styles.chip}>
-        {chips.map((chip) => (
-          <Chip
-            key={chip.id}
-            onPress={() => handleChipPress(chip.id)}
-            mode={chip.selected ? "flat" : "outlined"}
-            selected={chip.selected}
-            style={{ margin: 5 }}
-          >
-            {chip.text}
-          </Chip>
-        ))}
-      </View>
-      <Text style={styles.text}>O ingrese etiquetas separadas por coma:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Etiqueta1, Etiqueta2, ..."
-        value={inputTags}
-        onChangeText={handleTagsInputChange}
-      />
-      <View style={styles.containerButton}>
-        <Button title={"Clasificador"} onPress={handleUpload} />
-      </View>
-      <View style={styles.response}>
-        <Text style={styles.text}>{result}</Text>
-      </View>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-  },
-  containerButton: {
-    width: "100%",
-    marginBottom: 20,
-  },
-  chip: {
-    marginBottom: "3%",
-    flexDirection: "row",
-    justifyContent: "center",
-    flexWrap: "wrap",
-  },
-  input: {
-    height: 40,
-    width: "80%",
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 20,
-    padding: 10,
-  },
-  text: {
-    fontSize: 14,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: "3%",
-  },
-  response: {
-    padding: 50,
-  },
+    input: {
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 10,
+        padding: 10,
+        marginVertical: 10,
+        width: '80%',
+        fontSize: 16,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#f5f5f5',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 20,
+    },
+    text: {
+      fontSize: 18,
+      marginBottom: 10,
+      marginTop: 10,
+      color: '#333',
+      textAlign: 'center',
+    },
+    button: {
+      backgroundColor: '#6200EE',
+      padding: 15,
+      borderRadius: 10,
+      alignItems: 'center',
+      marginTop: 20,
+      width: '80%',
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 18,
+      fontWeight: 'bold',
+      alignContents: 'center',
+    },
+    result: {
+      fontSize: 18,
+      marginTop: 20,
+      color: '#333',
+      textAlign: 'center',
+    },
+    resultContainer: {
+        marginVertical: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 10,
+        width: '100%',
+    },
 });
-
-export default Classifier;
+export default Order;
